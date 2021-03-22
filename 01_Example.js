bf10c9fa448aae3261e2e0410fb49fd3602a821e
maeng2418 <maeng2418@naver.com>
2021-03-23 08:13:05 +0900

    [refactor] 변수 인라인

--- 01_Example.js
+++ 01_Example.js
@@ -53,16 +53,15 @@ const statement = (invoice, plays) => {
   };
 
   for (let perf of invoice.performances) {
-    const play = playFor(perf);
-    let thisAmount = amountFor(perf, play);
+    let thisAmount = amountFor(perf, playFor(perf));
 
     // 포인트 적립
     volumeCredits += Math.max(perf.audience - 30, 0);
     // 희극 관객 5명마다 추가 포인트 제공
-    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if ('comedy' === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
     // 청구 내역을 출력한다.
-    result += `${play.name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`;
+    result += `${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`;
     totalAmount += thisAmount;
   }
 
